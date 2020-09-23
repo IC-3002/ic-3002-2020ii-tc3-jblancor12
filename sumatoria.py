85d6201902d063e0bb05d42eb9a09624897b3ac6
jblancor12 <frikimcpelon@gmail.com>
2020-09-23 11:31:50 -0600

    TareaCorta3

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,20 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+	res=0
+	for i in range(n):	
+		
+		for j in range(i+1):
+			
+			for k in range(i+1):
+				res=res+1	
+	print (res)
+	return res
+
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
+	print ((n*(n+1)*(2*n+1))/6)
+	return (n*(n+1)*(2*n+1))/6
 
+
+
+sumatoria_constante(10)
+sumatoria_cubica(10)
